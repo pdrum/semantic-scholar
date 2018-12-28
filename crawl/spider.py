abf246ba7819d8567b524e7f2351b0750397d7c8
pdrum <pedram.esmaeeli@gmail.com>
2018-12-28 22:37:35 +0330

    feat: Up and running crawler

--- crawl/spider.py
+++ crawl/spider.py
@@ -1,4 +1,5 @@
 from scrapy import Spider, Selector
+from scrapy.http import Response
 from selenium import webdriver
 from selenium.webdriver import FirefoxProfile
 from selenium.webdriver.firefox.options import Options
@@ -36,23 +37,34 @@ class PaperSpider(Spider):
         'https://www.semanticscholar.org/paper/Deep-Private-Feature-Extraction-Ossia-Taheri/3355aff37b5e4ba40fc689119fb48d403be288be',
     ]
 
+    def __init__(self):
+        super(PaperSpider, self).__init__()
+        self._crawled_ids = set()
+        self.limit = 2000
+        self._expand_details = False
+
     def parse(self, response):
-        selector = self._expanded_selector(response.url)
-        self.logger.info(self._extract_abstract(selector))
-        self.logger.info(self._extract_authors(selector))
-        self.logger.info(self._extract_year(selector))
-        self.logger.info(self._extract_title(selector))
-        self.logger.info('AH' * 10)
-        self.logger.info(self._extract_references(selector))
-        yield {
+        selector = self.get_selector(response)
+        if self._url_to_id(response.url) in self._crawled_ids:
+            return
+        if len(self._crawled_ids) == self.limit:
+            return
+
+        references = self._extract_references(selector)
+        result = {
             'type': 'paper',
             'id': self._url_to_id(response.url),
             'title': self._extract_title(selector),
             'authors': self._extract_authors(selector),
             'date': self._extract_year(selector),
             'abstract': self._extract_abstract(selector),
-            'references': self._extract_references(selector),
+            'references': [self._extract_reference_name(ref) for ref in references],
         }
+        self._crawled_ids.add(self._url_to_id(response.url))
+        yield result
+        for ref in references[:5]:
+            self.logger.info(ref)
+            yield response.follow(ref, callback=self.parse)
 
     def _extract_title(self, selector: Selector):
         return selector.css('[data-selenium-selector="paper-detail-title"]::text').extract_first()
@@ -71,17 +83,22 @@ class PaperSpider(Spider):
         ).extract_first()
 
     def _extract_references(self, selector: Selector):
-        links = selector.css(
+        return selector.css(
             '#references .citation .result-meta [data-selenium-selector="title-link"]::attr(href)'
         ).extract()
-        return [self._url_to_id(l) for l in links]
+
+    def _extract_reference_name(self, ref_url):
+        after_paper = ref_url.split('/paper/')[1]
+        return after_paper.split('/')[0]
 
     def _url_to_id(self, url: str) -> str:
         return url.split('/paper/')[1]
 
-    def _expanded_selector(self, url) -> Selector:
+    def get_selector(self, response: Response) -> Selector:
+        if not self._expand_details:
+            return Selector(text=response.text)
         with Firefox() as driver:
-            driver.get(url)
+            driver.get(response.url)
             self._click(driver, '.abstract__text .text-truncator__toggle')
             self._click(driver, '.more-authors-label')
             return Selector(text=driver.page_source)
@@ -90,4 +107,4 @@ class PaperSpider(Spider):
         try:
             driver.find_element_by_css_selector(selector).click()
         except Exception:
-            print('Failed to find {}'.format(selector))
+            self.logger.info('Failed to find {}'.format(selector))
