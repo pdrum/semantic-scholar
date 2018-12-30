d60dc6006e25a84a1b0f084f71b88f260b6c5095
pdrum <pedram.esmaeeli@gmail.com>
2018-12-30 22:55:34 +0330

    feat: Build elastic index

--- elastic/index.py
+++ elastic/index.py
@@ -1,2 +1,24 @@
-def fill_index():
-    pass
+import json
+
+from elasticsearch import Elasticsearch
+from elasticsearch.helpers import bulk
+
+
+def read_iterate():
+    with open('../crawl/crawl.json') as file:
+        data = json.load(file)
+        for paper in data:
+            yield {
+                '_index': 'semanticscholar',
+                '_type': 'paper',
+                'doc': paper
+            }
+
+
+def build_index():
+    es = Elasticsearch()
+    bulk(es, read_iterate())
+
+
+if __name__ == '__main__':
+    build_index()
---
+++ index.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+cd elastic
+python index.py
